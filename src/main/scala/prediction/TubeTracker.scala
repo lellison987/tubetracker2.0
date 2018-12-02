05f67c349b09c584c965eabbdcaed21f878a0b7b
John Gibson <johngibson@wustl.edu>
2018-12-02 14:22:11 -0600

    Added image labelling

--- src/main/scala/prediction/TubeTracker.scala
+++ src/main/scala/prediction/TubeTracker.scala
@@ -5,7 +5,8 @@ import java.awt.image.BufferedImage
 
 import akka.actor.ActorSystem
 import akka.util.Timeout
-import com.sksamuel.scrimage.Image
+import com.sksamuel.scrimage.{Image, Pixel}
+import filters.Filter
 import objects.{ImageTrackerOptions, ImageTubeList, Tubule}
 import processing.ImageProcessor
 
@@ -62,6 +63,23 @@ object TubeTracker {
     val fut = proc.processImages(imgs map { i => Image.fromAwt(i) })
     val images = Await.result(fut, Duration.Inf)
     images
+  }
+
+  def labelImages(vec: Vector[ImageTubeList]): Vector[Image] = {
+    vec map { case ImageTubeList(img, tubes) =>
+      val newimg = img.copy
+      tubes filter { tube => tube.points.nonEmpty } foreach { tube =>
+        val mbp = LinearRegression.doRegression(tube.points)
+        //println(s"Modelled tube with significance ${mbp._3} from ${tube.p1} to ${tube.p2} and length ${math.sqrt(math.pow(tube.p1._2 - tube.p2._2, 2) + math.pow(tube.p1._1 - tube.p2._1, 2))}")
+        val regpts = LinearRegression.produceConstrainedLinePoints(tube.points, mbp)
+        regpts foreach {
+          case (x, y) => if (Filter.checkPxRange(img, (x, y))) {
+            newimg.setPixel(x, y, Pixel(255, 0, 0, img.pixel(x, y).alpha))
+          }
+        }
+      }
+      newimg
+    }
   }
 
   /*
